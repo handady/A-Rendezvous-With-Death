b1d057ff8e9d0e3242d0994b6193040ef8ba10cf
Hans <162235990@qq.com>
2024-04-24 15:31:37 +0800

    初始化项目

---
+++ main.js
@@ -0,0 +1,27 @@
+// main.js
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  // 加载Vue应用的入口HTML
+  win.loadURL("http://localhost:8080");
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
